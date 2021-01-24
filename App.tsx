5f876b6b01a5f9fe1e10d01cb27b5bb71e209f24
Gustavo Baptista <gustavogsb@outlook.com>
2021-01-24 12:39:19 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.text}>Wellcome to Buglândia!!</Text>
+        <Text style={styles.p}>A Minecraft Server</Text>
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#121214',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#8257e6',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  p: {
+    color: '#e1e1e6',
+    fontSize: 18,
+    // fontWeight: 'bold',
+  }
 });
